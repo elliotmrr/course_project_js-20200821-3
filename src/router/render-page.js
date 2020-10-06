d90eb63e3cceebc1d1f5367afa316db8eafe5566
Elliot <elliotpurouski@gmail.com>
2020-10-06 04:01:39 +0300

    Render-page: removed await before page.render()

--- src/router/render-page.js
+++ src/router/render-page.js
@@ -4,15 +4,15 @@ export default async function(path, match) {
   main.classList.add('is-loading');
 
   const { default: Page } = await import(/* webpackChunkName: "[request]" */`../pages/${path}/index.js`);
-  const page = new Page();
-  const element = await page.render();
-
-  main.classList.remove('is-loading');
+  const page = new Page(match);
+  const element = page.render();
 
   const contentNode = document.querySelector('#content');
 
   contentNode.innerHTML = '';
   contentNode.append(element);
+
+  main.classList.remove('is-loading');
 
   return page;
 }
